38d5161e504165a298be9dd981bd7ff9ca109deb
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-24 00:42:56 +0900

    input配列作成時のブロック変数名を変更

--- paiza.rb
+++ paiza.rb
@@ -9,8 +9,8 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   input_lines = 1
-  input = input_lines.times.inject([]) do |line|
-    line << gets.chomp
+  input = input_lines.times.inject([]) do |arr|
+    arr << gets.chomp
   end
   puts Paiza.new(*input).answer
 end
