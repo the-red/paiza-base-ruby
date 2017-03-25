bd5aafe19b22bb12fa697158d38e09adcaab12f0
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-25 14:44:26 +0900

    入出力をヒアドキュメントでの記述に変更

--- spec/paiza_spec.rb
+++ spec/paiza_spec.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 describe Paiza do
   it do
-    paiza = Paiza.new(
-      'line1',
-      'line2'
-    )
-    expect(paiza.answer).to eq('output')
+    input = <<EOS
+Input1
+Input2
+EOS
+    output = <<EOS
+Output1
+Output2
+EOS
+    paiza = Paiza.new(*input.split("\n"))
+    expect(paiza.answer).to eq(output)
   end
 end
