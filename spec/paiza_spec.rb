079a129dd433e03bed586ab15430d12f9e13a080
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-25 15:33:20 +0900

    ヒアドキュメントのインデント許可

--- spec/paiza_spec.rb
+++ spec/paiza_spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 describe Paiza do
   it do
-    input = <<EOS
-Input1
-Input2
-EOS
-    output = <<EOS
-Output1
-Output2
-EOS
+    input = <<~EOS
+    Input1
+    Input2
+    EOS
+    output = <<~EOS
+    Output1
+    Output2
+    EOS
     paiza = Paiza.new(*input.split("\n"))
-    expect(paiza.answer).to eq(output)
+    expect(paiza.answer).to eq(output.chomp)
   end
 end
